60da1b617443e32ed42f2f6e69898dda99b38a3b
Jeroen Rijsdijk <88040406+Jeroen-TheCodingFactory@users.noreply.github.com>
2022-11-22 18:18:18 +0100

    refactored to hooks (#1)

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,42 +1,19 @@
 import Item from "../Item/Item";
 import "./Todo.css";
 
-const Todo = () => {
-
-    const tasks = [
-        {
-            name: "Groceries",
-            done: false,
-        },
-        {
-            name: "Walking the dog",
-            done: true,
-        },
-        {
-            name: "Message Girlfriend",
-            done: false,
-        },
-        {
-            name: "Code awesome stuff",
-            done: false,
-        },
-        {
-            name: "Code more awesome stuff!",
-            done: false,
-        }
-    ];
-
-    let listItems = tasks.map(task => {
-        return <Item name={task.name} done={task.done}/>
-    });
+const Todo = (props) => {
+    
+    let toBeRenderedItems = props.items.map(item => {
+        return <Item key={item.name + "" + item.done} name={item.name} done={item.done} />
+    })
 
     return (
         <article className="todo">
-            <header class="todo__header">
-                <h1 class="todo__heading">Things to do:</h1>
+            <header className="todo__header">
+                <h1 className="todo__heading">{props.title}</h1>
             </header>
             <ul className="todo__list">
-                {listItems}
+                {toBeRenderedItems}
             </ul>
         </article>
     );
